20b66d468c1eacd01b911bf0aba70169f19a9195
delmoromatias <delmoromatias@gmail.com>
2024-05-19 22:01:54 -0300

    ejercitacion 10

---
+++ script.js
@@ -0,0 +1,17 @@
+let nombre= "Pedro";
+let apellido= "Gonzales";
+console.log(nombre + " " + apellido);
+let numberone=120;
+let numbertwo=20;
+let resultsuma = numberone + numbertwo;
+let resultresta = numberone - numbertwo;
+let resultmulti = numberone * numbertwo;
+let resultdivision = numberone / numbertwo;
+console.log("suma:" , resultsuma);
+console.log("resta:" , resultresta);
+console.log("multiplicacion:", resultmulti);
+console.log("division:", resultdivision);
+let comp = numberone > numbertwo;
+console.log(comp);
+let numberthree = resultsuma + 720;
+console.log("suma-variable+numero:" ,numberthree);
